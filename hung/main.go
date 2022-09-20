a3be4157951ce0b836e320ac89886e2ae1085dcd
kirill klimenko <kirill_klimenko@mail.ru>
2022-09-20 16:30:38 +0300

    all work, without ending

--- hung/main.go
+++ hung/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 var words = []string{
 	"helloz",
-	"buzy",
+	"buzzy",
 	"zombie",
 	"zoom",
 	"mangoz",
@@ -20,36 +23,35 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	word := words[rand.Intn(len(words))]
 	mistakes := 0
-	var ch rune
+	//var ch byte
+	//ch = 'z'
 	//var wordMap map[rune]bool
-	wordMap := map[rune]bool{}
-
-	for _, w := range word {
-		if w == ' ' {
-			wordMap[w] = true
-		} else {
-			wordMap[w] = false
-		}
+	wordMap := map[string]bool{}
+	//fmt.Scan(&ch)
+	//fmt.Println(ch)
+	for mistakes <= 8 {
+		getChar(wordMap, word, &mistakes)
+		print(mistakes, wordMap, word)
 	}
-	fmt.Scan(&ch)
-	fmt.Println(ch)
-	getChar(wordMap, &mistakes, ch)
-	print(mistakes, wordMap, word)
-
 }
 
-func print(mistakes int, wordMap map[rune]bool, word string) {
+func print(mistakes int, wordMap map[string]bool, word string) {
 	fmt.Print("Your word is : ")
 	for _, b := range word {
-		if !wordMap[b] {
-			fmt.Print("_ ")
+		if b == ' ' {
+			fmt.Print(" ")
+		} else if wordMap[string(b)] {
+			fmt.Print(string(b))
+
 		} else {
-			fmt.Printf("%c ", b)
+			fmt.Print("_")
 		}
+		fmt.Print(" ")
 	}
 	fmt.Print("\n")
-	path := "c:/Projects For Prog/Golang/hello world/hungman/2"
-	//path += strconv.Itoa(mistakes)
+	path := "hungman/"
+	path += strconv.Itoa(mistakes)
+
 	/*f, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -60,19 +62,36 @@ func print(mistakes int, wordMap map[rune]bool, word string) {
 		}
 	}()*/
 	b, err := os.ReadFile(path)
+
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Print(string(b))
 }
 
-func getChar(wordMap map[rune]bool, mistakes *int, ch rune) {
-	fmt.Println("Write your letter: ", ch)
+func getChar(wordMap map[string]bool, word string, mistakes *int) {
+	fmt.Println("Write your letter: ")
+	r := bufio.NewReader(os.Stdin)
+	ch, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	ch = strings.TrimSpace(ch)
+	if len(ch) != 1 || ch == " " {
+		for len(ch) != 1 {
+			fmt.Println("Error, Write only 1 letter")
+			ch, err = r.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+			ch = strings.TrimSpace(ch)
+		}
+	}
+
 	flag := 0
-	for c, b := range wordMap {
-		if c == ch && !b {
-			wordMap[c] = true
+	for _, b := range word {
+		if ch == string(b) {
+			wordMap[ch] = true
 			flag = 1
 		}
 	}
@@ -82,4 +101,17 @@ func getChar(wordMap map[rune]bool, mistakes *int, ch rune) {
 	} else {
 		fmt.Println("Okey, we have this letter in word.")
 	}
+
+	/*for c, b := range wordMap {
+		if c == rune(ch[0]) && !b {
+			wordMap[c] = true
+			flag = 1
+		}
+	}
+	if flag == 0 {
+		fmt.Println("I'ts misstake !")
+		*mistakes++
+	} else {
+		fmt.Println("Okey, we have this letter in word.")
+	}*/
 }
