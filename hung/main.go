73269df29a8ee5235fb8dbdd55fce85a0c3362aa
kirill klimenko <kirill_klimenko@mail.ru>
2022-09-20 16:56:48 +0300

    done

--- hung/main.go
+++ hung/main.go
@@ -23,25 +23,27 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	word := words[rand.Intn(len(words))]
 	mistakes := 0
-	//var ch byte
-	//ch = 'z'
-	//var wordMap map[rune]bool
 	wordMap := map[string]bool{}
-	//fmt.Scan(&ch)
-	//fmt.Println(ch)
-	for mistakes <= 8 {
+	for mistakes < 8 {
 		getChar(wordMap, word, &mistakes)
-		print(mistakes, wordMap, word)
+		print(&mistakes, wordMap, word)
+	}
+	if mistakes == 8 {
+		println("GAMEOVER YOU LOSE")
+	} else {
+		println("YOU WIN!!")
 	}
 }
 
-func print(mistakes int, wordMap map[string]bool, word string) {
+func print(mistakes *int, wordMap map[string]bool, word string) {
+	comparestr := ""
 	fmt.Print("Your word is : ")
 	for _, b := range word {
 		if b == ' ' {
 			fmt.Print(" ")
 		} else if wordMap[string(b)] {
 			fmt.Print(string(b))
+			comparestr += string(b)
 
 		} else {
 			fmt.Print("_")
@@ -50,23 +52,17 @@ func print(mistakes int, wordMap map[string]bool, word string) {
 	}
 	fmt.Print("\n")
 	path := "hungman/"
-	path += strconv.Itoa(mistakes)
+	path += strconv.Itoa(*mistakes)
 
-	/*f, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err = f.Close(); err != nil {
-			panic(err)
-		}
-	}()*/
 	b, err := os.ReadFile(path)
 
 	if err != nil {
 		panic(err)
 	}
 	fmt.Print(string(b))
+	if comparestr == word {
+		*mistakes = 9
+	}
 }
 
 func getChar(wordMap map[string]bool, word string, mistakes *int) {
@@ -102,16 +98,4 @@ func getChar(wordMap map[string]bool, word string, mistakes *int) {
 		fmt.Println("Okey, we have this letter in word.")
 	}
 
-	/*for c, b := range wordMap {
-		if c == rune(ch[0]) && !b {
-			wordMap[c] = true
-			flag = 1
-		}
-	}
-	if flag == 0 {
-		fmt.Println("I'ts misstake !")
-		*mistakes++
-	} else {
-		fmt.Println("Okey, we have this letter in word.")
-	}*/
 }
